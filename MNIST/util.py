cee90e4b5247c029ed61be5d6ac8ca7a3ba68cef
ichase5 <35161374+ichase5@users.noreply.github.com>
2018-07-09 11:40:33 +0800

    Update util.py

--- MNIST/util.py
+++ MNIST/util.py
@@ -3,18 +3,16 @@ import numpy
 
 class BinOp():
     def __init__(self, model):
-        # count the number of Conv2d and Linear
-        count_targets = 0
+        
+        count_targets = 0   # count the number of Conv2d and Linear
         for m in model.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
                 count_targets = count_targets + 1
 
         start_range = 1
         end_range = count_targets-2
-        self.bin_range = numpy.linspace(start_range,
-                end_range, end_range-start_range+1)\
-                        .astype('int').tolist()
-        self.num_of_params = len(self.bin_range)
+        self.bin_range = numpy.linspace(start_range,end_range, end_range-start_range+1).astype('int').tolist() # [1,2,...,end_range]
+        self.num_of_params = len(self.bin_range)  #要二值化的参数个数
         self.saved_params = []
         self.target_modules = []
         index = -1
@@ -23,9 +21,9 @@ class BinOp():
                 index = index + 1
                 if index in self.bin_range:
                     tmp = m.weight.data.clone()
-                    self.saved_params.append(tmp)
-                    self.target_modules.append(m.weight)
-        return
+                    self.saved_params.append(tmp)  #存储要二值化的参数的data，是tensor
+                    self.target_modules.append(m.weight) #这里是Variable
+        return 
 
     def binarization(self):
         self.meancenterConvParams()
@@ -33,39 +31,38 @@ class BinOp():
         self.save_params()
         self.binarizeConvParams()
 
-    def meancenterConvParams(self):
-        for index in range(self.num_of_params):
+    def meancenterConvParams(self):  #将要二值化的权重矩阵按行0均值化
+        for index in range(self.num_of_params):  
             s = self.target_modules[index].data.size()
-            negMean = self.target_modules[index].data.mean(1, keepdim=True).\
-                    mul(-1).expand_as(self.target_modules[index].data)
+            negMean = self.target_modules[index].data.mean(1, keepdim=True).mul(-1).expand_as(self.target_modules[index].data)
             self.target_modules[index].data = self.target_modules[index].data.add(negMean)
 
-    def clampConvParams(self):
+    def clampConvParams(self): # 将要二值化的权重矩阵 裁剪到-1到1之间
         for index in range(self.num_of_params):
             self.target_modules[index].data.clamp(-1.0, 1.0,
                     out = self.target_modules[index].data)
 
-    def save_params(self):
+    def save_params(self):  #将在self.target_modules[index].data中处理过的数据复制到self.saved_params[index]中去
         for index in range(self.num_of_params):
             self.saved_params[index].copy_(self.target_modules[index].data)
 
-    def binarizeConvParams(self):
+    def binarizeConvParams(self):  #权重二值化函数
         for index in range(self.num_of_params):
-            n = self.target_modules[index].data[0].nelement()
+            n = self.target_modules[index].data[0].nelement()   #卷积则是卷积核中的元素个数，全连接则是权重向量个数
             s = self.target_modules[index].data.size()
-            if len(s) == 4:
-                m = self.target_modules[index].data.norm(1, 3, keepdim=True)\
-                        .sum(2, keepdim=True).sum(1, keepdim=True).div(n)
-            elif len(s) == 2:
+            if len(s) == 4: #卷积层
+                m = self.target_modules[index].data.norm(1, 3, keepdim=True).sum(2, keepdim=True).sum(1, keepdim=True).div(n)
+                #norm(1,3,keepdim=True ) 1代表L1范数，3代表对第3维度求范数   由batch-c-h-w 变成了 1-c-h-w
+                #接着对channle求和, 对height求和 求和不改变shape
+            elif len(s) == 2: #全连接层
                 m = self.target_modules[index].data.norm(1, 1, keepdim=True).div(n)
-            self.target_modules[index].data.sign()\
-                    .mul(m.expand(s), out=self.target_modules[index].data)
+            self.target_modules[index].data.sign().mul(m.expand(s), out=self.target_modules[index].data)    #二值化
 
-    def restore(self):
+    def restore(self):  #将在self.saved_params[index]中的数据复制到self.target_modules[index].data中去
         for index in range(self.num_of_params):
             self.target_modules[index].data.copy_(self.saved_params[index])
 
-    def updateBinaryGradWeight(self):
+    def updateBinaryGradWeight(self): 
         for index in range(self.num_of_params):
             weight = self.target_modules[index].data
             n = weight[0].nelement()
